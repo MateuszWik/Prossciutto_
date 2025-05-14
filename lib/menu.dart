3dcb7a389442340fb6e2e2ff47bca17ce7c9e539
Hubert Kani <hubertkani123@gmail.com>
2025-05-14 13:45:27 +0200

    a

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,17 +30,104 @@ class MyMenuPage extends StatefulWidget {
 }
 
 class _MyMenuPageState extends State<MyMenuPage> {
-
+  var selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Hi'),
-        titleTextStyle: TextStyle(
-          fontSize: 15,
-          fontFamily: 'LeagueSpartan',
-        ),
-      )
+    var colorScheme = Theme.of(context).colorScheme;
+
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = Placeholder();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      case 2:
+        page = Placeholder();
+      case 3:
+        page = Placeholder();
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    var mainArea = ColoredBox(
+      color: colorScheme.surfaceContainerHighest,
+      child: AnimatedSwitcher(
+        duration: Duration(milliseconds: 200),
+        child: page,
+      ),
     );
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(child: mainArea),
+
+          // Pasek dolny jako warstwa
+          Positioned(
+            left: 16,
+            right: 16,
+            bottom: 60,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: Material(
+                elevation: 10,
+                child: Container(
+                  height: 60,
+                  color: Color(0xFF0C8C75),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      IconButton(
+                        icon: Icon(selectedIndex == 0 ? Icons.home : Icons.home_outlined,),
+                        color: Colors.white,
+                        onPressed: () {
+                          setState(() {
+                            selectedIndex = 0;
+                          });
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(selectedIndex == 1 ? Icons.favorite : Icons.favorite_border_outlined),
+                        color: Colors.white,
+                        onPressed: () {
+                          setState(() {
+                            selectedIndex = 1;
+
+                          });
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(selectedIndex == 2 ? Icons.shopping_cart : Icons.shopping_cart_outlined),
+                        color: Colors.white,
+                        onPressed: () {
+                          setState(() {
+                            selectedIndex = 2;
+                          });
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(selectedIndex == 3 ? Icons.person_2 : Icons.person_2_outlined),
+                        color: Colors.white,
+                        onPressed: () {
+                          setState(() {
+                            selectedIndex = 3;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+
+
   }
+
+
 }
