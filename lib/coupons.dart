7aac4e39a4094565e155cd3528b40822004c8af6
Amelia Golebiewska <ameliagoleb437@op.pl>
2025-05-15 13:59:47 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	lib/main.dart

--- lib/coupons.dart
+++ lib/coupons.dart
@@ -1,115 +1,75 @@
 import 'package:flutter/material.dart';
-import './favorites.dart';
-import './main.dart';
 import './account.dart';
 import './cart.dart';
+import './main.dart';
+import './favorites.dart';
 
-void main() => runApp(CouponsApp());
+void main() {
+  runApp(MyApp());
+}
 
-class CouponsApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Coupons(),
       debugShowCheckedModeBanner: false,
+      title: "Coupons",
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFFF3ECE4)),
+      ),
+      home: Menu(),
     );
   }
 }
 
 class Coupons extends StatefulWidget {
   @override
-  State<Coupons> createState() => _CouponsScreenState();
+  _Coupons createState() => _Coupons();
 }
 
-class _CouponsScreenState extends State<Coupons> {
-  final Color mainGreen = Color(0xFF008065);
-  int selectedIndex = 0;
-
-  Widget buildCoupon(String title, String discount, {String? subtitle}) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      padding: EdgeInsets.all(12),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Expanded(child: Text(title, style: TextStyle(fontWeight: FontWeight.bold))),
-              Icon(Icons.add, size: 20),
-            ],
-          ),
-          SizedBox(height: 6),
-          Text(discount, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: mainGreen)),
-          if (subtitle != null)
-            Text(subtitle, style: TextStyle(fontSize: 12, color: Colors.grey[700])),
-        ],
-      ),
-    );
-  }
-
-  Widget buildCouponsBody() {
-    return Column(
-      children: [
-        Expanded(
-          child: Container(
-            decoration: BoxDecoration(
-              color: Color(0xFF0C8C75),
-              borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
-            ),
-            padding: EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Permanent discounts", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-                    Text("Weekly discounts", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Expanded(
-                  child: GridView.count(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 12,
-                    crossAxisSpacing: 12,
-                    childAspectRatio: 1.2,
-                    children: [
-                      buildCoupon("Student discount", "30% OFF", subtitle: "Below 25y old"),
-                      buildCoupon("2nd pizza", "50% OFF"),
-                      buildCoupon("User discount", "5% OFF"),
-                      buildCoupon("All pasta", "2% OFF"),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget getPageByIndex(int index) {
-    if (index == 0) return Menu();
-    if (index == 1) return Favorites();
-    if (index == 2) return Cart();
-    if (index == 3) return Account();
-    return Center(child: Text("Unknown page"));
-  }
+class _Coupons extends State<Menu> {
+  var selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    var colorScheme = Theme.of(context).colorScheme;
+
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = Menu();
+        break;
+      case 1:
+        page = Favorites();
+        break;
+      case 2:
+        page = Cart();
+        break;
+      case 3:
+        page = Account();
+        break;
+      case 4:
+        page = Menu();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    var mainArea = ColoredBox(
+      color: colorScheme.surfaceContainerHighest,
+      child: AnimatedSwitcher(
+        duration: Duration(milliseconds: 200),
+        child: page,
+      ),
+    );
+
     return Scaffold(
-      backgroundColor: Color(0xFFF3ECE4),
       body: Stack(
         children: [
-          Positioned.fill(
-            child: getPageByIndex(selectedIndex),
-          ),
+          Positioned.fill(child: mainArea),
+
           Positioned(
             left: 16,
             right: 16,
@@ -120,37 +80,196 @@ class _CouponsScreenState extends State<Coupons> {
                 elevation: 10,
                 child: Container(
                   height: 60,
-                  color: Color(0xFF0C8C75),
+                  color: Color(0xFFF3ECE4),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
                       IconButton(
                         icon: Icon(selectedIndex == 0 ? Icons.home : Icons.home_outlined),
-                        color: Colors.white,
-                        onPressed: () => setState(() => selectedIndex = 0),
+                        color: Color(0XFF004D40),
+                        onPressed: () {
+                          setState(() {
+                            selectedIndex = 0;
+                          });
+                        },
                       ),
                       IconButton(
                         icon: Icon(selectedIndex == 1 ? Icons.favorite : Icons.favorite_border_outlined),
-                        color: Colors.white,
-                        onPressed: () => setState(() => selectedIndex = 1),
+                        color: Color(0XFF004D40),
+                        onPressed: () {
+                          setState(() {
+                            selectedIndex = 1;
+                          });
+                        },
                       ),
                       IconButton(
                         icon: Icon(selectedIndex == 2 ? Icons.shopping_cart : Icons.shopping_cart_outlined),
-                        color: Colors.white,
-                        onPressed: () => setState(() => selectedIndex = 2),
+                        color: Color(0XFF004D40),
+                        onPressed: () {
+                          setState(() {
+                            selectedIndex = 2;
+                          });
+                        },
                       ),
                       IconButton(
-                        icon: Icon(selectedIndex == 3 ? Icons.person : Icons.person_outline),
-                        color: Colors.white,
-                        onPressed: () => setState(() => selectedIndex = 3),
+                        icon: Icon(selectedIndex == 3 ? Icons.person_2 : Icons.person_2_outlined),
+                        color: Color(0XFF004D40),
+                        onPressed: () {
+                          setState(() {
+                            selectedIndex = 3;
+                          });
+                        },
                       ),
                     ],
                   ),
                 ),
               ),
             ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  final VoidCallback onCouponTap;
+
+  const HomeScreen({super.key, required this.onCouponTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    'Hi',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Spacer(),
+                  InkWell(
+                    onTap: onCouponTap,
+                    child: Image.asset('assets/images/kupon.png'),
+                  ),
+                ],
+              ),
+              SizedBox(height: 12),
+              RichText(
+                text: TextSpan(
+                  text: "It's not just ",
+                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Colors.black),
+                  children: [
+                    TextSpan(text: "Food", style: TextStyle(color: Colors.teal)),
+                    TextSpan(text: "\nIt's an "),
+                    TextSpan(text: "Experience", style: TextStyle(color: Colors.teal)),
+                  ],
+                ),
+              ),
+              SizedBox(height: 16),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'search...',
+                  prefixIcon: Icon(Icons.search),
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 16),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              SizedBox(height: 24),
+              Text('Pasta', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              SizedBox(height: 12),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    _buildFoodCard(
+                        imagePath: 'assets/images/Macaroni.png',
+                        name: 'Macaroni\nCampania',
+                        price: '20\$'
+                    ),
+                    SizedBox(width: 12),
+                    _buildFoodCard(
+                        imagePath: 'assets/images/Spaghetti-Sicily.png',
+                        name: 'Spaghetti\nSicily',
+                        price: '25\$'
+                    ),
+                    SizedBox(width: 12),
+                    _buildFoodCard(
+                        imagePath: 'assets/images/Penne_all_arrabbiata.png',
+                        name: 'Penne all\'\nArrabbiata',
+                        price: '25\$'
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 24),
+              Text('Pizza', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              SizedBox(height: 12),
+              Row(
+                children: [
+                  _buildEmptyCard(),
+                  SizedBox(width: 12),
+                  _buildEmptyCard(),
+                ],
+              ),
+              SizedBox(height: 100),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFoodCard({required String imagePath, required String name, required String price}) {
+    return Container(
+      width: 140,
+      height: 180,
+      padding: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(color: Colors.black12, blurRadius: 6, offset: Offset(0, 2)),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image.asset(imagePath, height: 80, width: double.infinity, fit: BoxFit.cover),
+          ),
+          SizedBox(height: 8),
+          Text(name, style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(price, style: TextStyle(fontWeight: FontWeight.bold)),
+              Icon(Icons.favorite_border_outlined, size: 18),
+            ],
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildEmptyCard() {
+    return Container(
+      width: 140,
+      height: 150,
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(16),
       ),
     );
   }
