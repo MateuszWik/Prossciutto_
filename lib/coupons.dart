1f894308e729a675da8bef0a09ea0f01e12eddf6
Amelia Golebiewska <ameliagoleb437@op.pl>
2025-05-19 10:05:44 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	lib/main.dart

--- lib/coupons.dart
+++ lib/coupons.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
-import './main.dart';
+
+import 'main.dart';
+
+// Pamiętaj, aby zaimportować swoją klasę Menu, np.:
+// import 'menu.dart';
+
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFFF3ECE4)),
+        useMaterial3: true,
       ),
       home: Coupons(),
-      debugShowCheckedModeBanner: false,
     ),
   );
 }
@@ -22,44 +28,28 @@ class Coupons extends StatefulWidget {
 class _CouponsState extends State<Coupons> {
   int selectedIndex = 4;
   final Color mainGreen = const Color(0xFF0C8C75);
-  final Color mainwhite = const Color(0xFFF3ECE4);
+  final Color mainWhite = const Color(0xFFF3ECE4);
 
   @override
   Widget build(BuildContext context) {
-    var colorScheme = Theme.of(context).colorScheme;
-
     Widget page;
+
     switch (selectedIndex) {
-      case 0:
-        page = Placeholder();
-        break;
-      case 1:
-        page = Placeholder();
-        break;
-      case 2:
-        page = Placeholder();
-        break;
-      case 3:
-        page = Placeholder();
-        break;
       case 4:
         page = _buildCouponsPage();
         break;
       default:
-        page = Placeholder();
+        page = Center(child: Text("Placeholder Page"));
     }
 
     return Scaffold(
-      backgroundColor: mainwhite,
+      backgroundColor: mainWhite,
       body: Stack(
         children: [
           Positioned.fill(
-            child: ColoredBox(
-              color: colorScheme.surfaceContainerHighest,
-              child: AnimatedSwitcher(
-                duration: Duration(milliseconds: 200),
-                child: page,
-              ),
+            child: AnimatedSwitcher(
+              duration: Duration(milliseconds: 300),
+              child: page,
             ),
           ),
           Positioned(
@@ -72,7 +62,7 @@ class _CouponsState extends State<Coupons> {
                 elevation: 10,
                 child: Container(
                   height: 60,
-                  color: mainwhite,
+                  color: mainWhite,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
@@ -111,7 +101,6 @@ class _CouponsState extends State<Coupons> {
     return SafeArea(
       child: Column(
         children: [
-          // App bar
           Padding(
             padding: const EdgeInsets.only(top: 40, left: 16, right: 16, bottom: 20),
             child: Stack(
@@ -125,99 +114,65 @@ class _CouponsState extends State<Coupons> {
                         context,
                         MaterialPageRoute(builder: (context) => Menu()),
                       );
-
                     },
-                    child: Image.asset("assets/images/left_arrow.png"),
+                    child: Image.asset("assets/images/left_arrow.png", width: 24, height: 24),
                   ),
                 ),
-                const Text(
-                  "Coupons",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontFamily: 'League Spartan',
-                    fontWeight: FontWeight.bold,
+                const Center(
+                  child: Text(
+                    "Coupons",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'League Spartan',
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ],
             ),
           ),
 
-          // Body with green background
           Expanded(
             child: Container(
               decoration: BoxDecoration(
                 color: mainGreen,
                 borderRadius: const BorderRadius.vertical(top: Radius.circular(30)),
               ),
-              padding: const EdgeInsets.all(30),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // Tabs
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text(
-                        "Permanent discounts",
-                        style: TextStyle(
-                          fontFamily: 'League Spartan',
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
+              padding: const EdgeInsets.all(20),
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  double width = constraints.maxWidth;
+                  int crossAxisCount = 2;
+                  double spacing = 25;
+                  double totalSpacing = spacing * (crossAxisCount - 1);
+                  double itemWidth = (width - totalSpacing) / crossAxisCount;
+                  double itemHeight = itemWidth * 0.75;
+
+                  return GridView.count(
+                    crossAxisCount: crossAxisCount,
+                    crossAxisSpacing: spacing,
+                    mainAxisSpacing: spacing,
+                    childAspectRatio: itemWidth / itemHeight,
+                    children: [
+                      _buildCouponCard(
+                        title: 'Student discount',
+                        description: '30% OFF\nBelow 25y old',
                       ),
-                      Text(
-                        "Weekly discounts",
-                        style: TextStyle(
-                          fontFamily: 'League Spartan',
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      _buildCouponCard(
+                        title: '2nd pizza',
+                        description: '50% OFF',
+                      ),
+                      _buildCouponCard(
+                        title: 'User discount',
+                        description: '5% OFF',
+                      ),
+                      _buildCouponCard(
+                        title: 'All pasta',
+                        description: '2% OFF',
                       ),
                     ],
-                  ),
-                  const SizedBox(height: 20),
-
-
-                  Expanded(
-                    child: LayoutBuilder(
-                      builder: (context, constraints) {
-                        double width = constraints.maxWidth;
-                        double height = constraints.maxHeight;
-
-                        int crossAxisCount = 2;
-                        double totalSpacing = 15 * (crossAxisCount - 1);
-                        double itemWidth = (width - totalSpacing) / crossAxisCount;
-                        double itemHeight = itemWidth * 0.75;
-                        double childAspectRatio = itemWidth / itemHeight;
-
-                        return GridView.count(
-                          crossAxisCount: crossAxisCount,
-                          mainAxisSpacing: 15,
-                          crossAxisSpacing: 15,
-                          childAspectRatio: childAspectRatio,
-                          children: [
-                            _buildCouponCard(
-                              title: 'Student discount',
-                              description: '30% OFF • Below 25y old',
-                            ),
-                            _buildCouponCard(
-                              title: '2nd pizza',
-                              description: '50% OFF',
-                            ),
-                            _buildCouponCard(
-                              title: 'User discount',
-                              description: '5% OFF',
-                            ),
-                            _buildCouponCard(
-                              title: 'All pasta',
-                              description: '2% OFF',
-                            ),
-                          ],
-                        );
-                      },
-                    ),
-                  ),
-                ],
+                  );
+                },
               ),
             ),
           ),
@@ -227,63 +182,67 @@ class _CouponsState extends State<Coupons> {
   }
 
   Widget _buildCouponCard({required String title, required String description}) {
-    return Stack(
-      children: [
-        Container(
-          padding: EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            color: mainwhite,
-            borderRadius: BorderRadius.circular(12),
-            boxShadow: [
-              BoxShadow(color: Colors.black12, blurRadius: 6, offset: Offset(0, 2)),
-            ],
-          ),
-          child: Row(
-            children: [
-              SizedBox(width: 12),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      title,
-                      style: TextStyle(
-                        color: mainGreen,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                        fontFamily: 'League Spartan',
-                      ),
-                    ),
-                    Text(
-                      description,
-                      style: TextStyle(
-                        color: Colors.grey[800],
-                        fontSize: 12,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-        Positioned(
-          top: 8,
-          right: 8,
-          child: Center(
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(color: Colors.black12, blurRadius: 6, offset: Offset(0, 2)),
+        ],
+      ),
+      child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 25),
+            decoration: BoxDecoration(
+              color: mainWhite,
+              borderRadius: BorderRadius.vertical(top: Radius.circular(12)),
+            ),
             child: Text(
-              '+',
+              title,
               style: TextStyle(
                 color: Colors.black,
                 fontWeight: FontWeight.bold,
-                fontSize: 20,
-                height: 1,
+                fontSize: 16,
+                fontFamily: 'League Spartan',
               ),
             ),
           ),
-        ),
-      ],
+          Expanded(
+            child: Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      description,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 13,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 10,
+                  right: 8,
+                  child: Text(
+                    "+",
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      height: 1,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
