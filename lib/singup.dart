ed33a0649cee84c3a8e5eda860ffd3db6728a703
mania <gardosmarijka@gmail.com>
2025-05-26 11:00:28 +0200

    zmiana

--- lib/singup.dart
+++ lib/singup.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: SignUpScreen(),
-    );//s
+    );
   }
 }
 
@@ -27,6 +27,7 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController nameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
@@ -34,7 +35,6 @@ class _SignUpScreenState extends State<SignUpScreen> {
   DateTime? selectedDate;
   final box = GetStorage();
   bool isPasswordVisible = false;
-
 
   void pickDate(BuildContext context) async {
     DateTime? pickedDate = await showDatePicker(
@@ -51,7 +51,6 @@ class _SignUpScreenState extends State<SignUpScreen> {
     }
   }
 
-
   bool isButtonEnabled = false;
 
   void checkFields() {
@@ -62,7 +61,6 @@ class _SignUpScreenState extends State<SignUpScreen> {
           selectedDate != null;
     });
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -84,140 +82,163 @@ class _SignUpScreenState extends State<SignUpScreen> {
           Center(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(height: 15),
-                  Padding(padding: EdgeInsets.only(top: 25),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(builder: (context) => Menu()),
-                        );
-                      },
-                      child: Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Image.asset(
-                          "assets/images/left_arrow.png",
-                          width: 24,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                  SizedBox(height: 80), //
-
-                  Text(
-                    'Sign Up',
-                    style: TextStyle(
-                      fontFamily: "LeagueSpartan",
-                      fontSize: 35,
-                      color: Colors.white,
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 13),
-                    child: TextFormField(
-                      controller: nameController,
-                      onChanged: (text) => checkFields(),
-                      decoration: InputDecoration(
-                        labelText: 'Name',
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 23),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 13),
-                    child: TextFormField(
-                      controller: emailController,
-                      onChanged: (text) => checkFields(),
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 23),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 13),
-                    child: TextFormField(
-                      controller: passwordController,
-                      onChanged: (text) => checkFields(),
-                      obscureText: !isPasswordVisible,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            isPasswordVisible ? Icons.visibility : Icons.visibility_off,
-                            color: Colors.grey,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              isPasswordVisible = !isPasswordVisible;
-                            });
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 15),
+                    Padding(
+                      padding: EdgeInsets.only(top: 25),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(builder: (context) => Menu()),
+                            );
                           },
+                          child: Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Image.asset(
+                              "assets/images/left_arrow.png",
+                              width: 24,
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-
-                  SizedBox(height: 23),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 13),
-                    child: InkWell(
-                      onTap: () => pickDate(context),
-                      child: Container(
+                    SizedBox(height: 80),
+                    Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        fontFamily: "LeagueSpartan",
+                        fontSize: 35,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 13),
+                      child: TextFormField(
+                        controller: nameController,
+                        onChanged: (text) => checkFields(),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your name';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'Name',
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 23),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 13),
+                      child: TextFormField(
+                        controller: emailController,
+                        onChanged: (text) => checkFields(),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your email';
+                          }
+                          if (!value.contains('@')) {
+                            return 'Email must contain "@"';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 23),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 13),
+                      child: TextFormField(
+                        controller: passwordController,
+                        onChanged: (text) => checkFields(),
+                        obscureText: !isPasswordVisible,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your password';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              isPasswordVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: Colors.grey,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                isPasswordVisible = !isPasswordVisible;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 23),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 13),
+                      child: InkWell(
+                        onTap: () => pickDate(context),
+                        child: Container(
+                          width: double.infinity,
+                          padding: EdgeInsets.symmetric(vertical: 16, horizontal: 13),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(16),
+                            border: Border.all(color: Colors.grey),
+                          ),
+                          child: Text(
+                            selectedDate == null
+                                ? "Select Date of Birth"
+                                : "${selectedDate!.year}-${selectedDate!.month}-${selectedDate!.day}",
+                            style: TextStyle(fontFamily: "MontSerrat", fontSize: 16),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 80),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 13),
+                      child: SizedBox(
+                        height: 65,
                         width: double.infinity,
-                        padding: EdgeInsets.symmetric(vertical: 16, horizontal: 13),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(16),
-                          border: Border.all(color: Colors.grey),
-                        ),
-                        child: Text(
-                          selectedDate == null
-                              ? "Select Date of Birth"
-                              : "${selectedDate!.year}-${selectedDate!.month}-${selectedDate!.day}",
-                          style: TextStyle(fontFamily: "MontSerrat", fontSize: 16),
-                        ),
-                      ),
-                    ),
-                  ),
-
-
-                  SizedBox(height: 80),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 13),
-                    child: SizedBox(
-                      height: 65,
-                      width: double.infinity,
-                      child: ElevatedButton(
+                        child: ElevatedButton(
                           onPressed: () {
-                            if (nameController.text.isEmpty ||
-                                emailController.text.isEmpty ||
-                                passwordController.text.isEmpty ||
-                                selectedDate == null) {
+                            // tutaj walidujemy formularz
+                            if (!_formKey.currentState!.validate()) {
+                              return;
+                            }
+                            if (selectedDate == null) {
                               ScaffoldMessenger.of(context).showSnackBar(
                                 SnackBar(
-                                  content: Text("Nie wszystkie pola są wypełnione!", style: TextStyle(color: Color(0xFF0C8C75))),
+                                  content: Text("Please select your date of birth", style: TextStyle(color: Color(0xFF0C8C75))),
                                   backgroundColor: Colors.white,
                                 ),
                               );
@@ -242,11 +263,9 @@ class _SignUpScreenState extends State<SignUpScreen> {
                               'name': nameController.text,
                               'email': emailController.text,
                               'password': passwordController.text,
-                            'dateOfBirth': selectedDate!.toIso8601String().split('T')[0],
+                              'dateOfBirth': selectedDate!.toIso8601String().split('T')[0],
                             };
                             box.write('userDateOfBirth', newUser['dateOfBirth']);
-
-
 
                             users.add(newUser);
                             box.write('users', users);
@@ -261,47 +280,48 @@ class _SignUpScreenState extends State<SignUpScreen> {
                               MaterialPageRoute(builder: (context) => Menu()),
                             );
                           },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.black,
-                          foregroundColor: Colors.white,
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(19)),
-                        ),
-                        child: Text(
-                          'Sign Up',
-                          style: TextStyle(fontFamily: "LeagueSpartan", color: Colors.white, fontSize: 20),
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 15),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Already have an account?",
-                        style: TextStyle(fontFamily: "MontSerrat", fontSize: 16, color: Colors.white),
-                      ),
-                      SizedBox(width: 5),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => LoginScreen()),
-                          );
-                        },
-                        child: Text(
-                          "Log in",
-                          style: TextStyle(
-                            fontFamily: "MontSerrat",
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black,
+                            foregroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(19)),
+                          ),
+                          child: Text(
+                            'Sign Up',
+                            style: TextStyle(fontFamily: "LeagueSpartan", color: Colors.white, fontSize: 20),
                           ),
                         ),
                       ),
-                    ],
-                  ),
-                ],
+                    ),
+                    SizedBox(height: 15),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Already have an account?",
+                          style: TextStyle(fontFamily: "MontSerrat", fontSize: 16, color: Colors.white),
+                        ),
+                        SizedBox(width: 5),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => LoginScreen()),
+                            );
+                          },
+                          child: Text(
+                            "Log in",
+                            style: TextStyle(
+                              fontFamily: "MontSerrat",
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
