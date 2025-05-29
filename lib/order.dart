8a1532e73ccd595efca3ed2923d288c6dbf984c9
Amelia Golebiewska <ameliagoleb437@op.pl>
2025-05-29 11:24:40 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	lib/main.dart

--- lib/order.dart
+++ lib/order.dart
@@ -7,47 +7,47 @@ class Order extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFF3ece4), // Beżowe tło
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                'Thank You for your order!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 24,
-                  fontFamily: 'LeagueSpartan',
-                ),
+      backgroundColor: const Color(0xFFF3ece4),
+      body: Center( // ← tutaj było: child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Thank You for your order!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 24,
+                fontFamily: 'LeagueSpartan',
               ),
-              SizedBox(height: 16),
-              Divider(
-                color: Colors.grey,
-                thickness: 2,
-                indent: 40,
-                endIndent: 40,
-              ),
-              SizedBox(height: 16),
-              Center(
-                child: InkWell(
-                  onTap: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => Menu()),
-                    );
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image.asset(
-                      "assets/images/left_arrow.png",
-                      width: 24,
-                    ),
+            ),
+            SizedBox(height: 16),
+            Divider(
+              color: Colors.grey,
+              thickness: 2,
+              indent: 40,
+              endIndent: 40,
+            ),
+            SizedBox(height: 16),
+            InkWell(
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => Menu()),
+                );
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Back to Home Screen",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
